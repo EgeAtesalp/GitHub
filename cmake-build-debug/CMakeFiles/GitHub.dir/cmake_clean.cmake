7bd0f685fd62a39e417b4abfa7a5cebbd2a29b1c
EgeAtesalp <egeatesalp@gmail.com>
2021-01-07 16:14:00 +0100

    ok

---
+++ cmake-build-debug/CMakeFiles/GitHub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHub.dir/main.c.o"
+  "GitHub"
+  "GitHub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
